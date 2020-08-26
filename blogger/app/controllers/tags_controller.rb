dc5c53c4699049c37ce973086ef8a169ed4788e2
Defoe.Brand <defoe.brand@gmail.com>
2020-08-26 18:32:09 +0300

    Create tag destroy, new, create methods

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -1,5 +1,4 @@
 class TagsController < ApplicationController
-
   def index
     @tags = Tag.all
   end
@@ -8,4 +7,24 @@ class TagsController < ApplicationController
     @tag = Tag.find(params[:id])
   end
 
+  def new
+    @tag = Tag.new
+  end
+
+  # def create
+  #   @tag = Tag.new(params[:id])
+  #   @tag.save
+  #
+  #   flash.notice = "Tag '#{@tag.name}' Created!"
+  #   # redirect_to article_path(@tag)
+  # end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' Destroyed!"
+
+    redirect_to article_path
+  end
 end
--- blogger/app/models/tag.rb
+++ blogger/app/models/tag.rb
@@ -1,8 +1,18 @@
 class Tag < ApplicationRecord
   has_many :taggings
-  has_many :articles, through: :taggings 
+  has_many :articles, through: :taggings
 
   def to_s
     name
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(',').collect { |s| s.strip.downcase }.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
+
+  def tag_list
+    articles.join(', ')
   end
 end
