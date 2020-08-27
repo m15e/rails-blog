82c0b4696c795ec4a330d416caaf859ed91578c0
Defoe.Brand <defoe.brand@gmail.com>
2020-08-27 12:58:46 +0300

    Adjust login credentials

--- blogger/app/controllers/authors_controller.rb
+++ blogger/app/controllers/authors_controller.rb
@@ -1,7 +1,7 @@
 class AuthorsController < ApplicationController
-  before_action :set_author, only: [:show, :edit, :update, :destroy]
-  before_action :zero_authors_or_authenticated, only: [:new, :create]
-  before_action :require_login, except: [:new, :create]
+  before_action :set_author, only: %i[show edit update destroy]
+  before_action :zero_authors_or_authenticated, only: %i[new create]
+  # before_action :require_login, except: %i[new create]
   # GET /authors
   # GET /authors.json
   def index
@@ -10,8 +10,7 @@ class AuthorsController < ApplicationController
 
   # GET /authors/1
   # GET /authors/1.json
-  def show
-  end
+  def show; end
 
   # GET /authors/new
   def new
@@ -19,8 +18,7 @@ class AuthorsController < ApplicationController
   end
 
   # GET /authors/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /authors
   # POST /authors.json
@@ -65,18 +63,19 @@ class AuthorsController < ApplicationController
   def zero_authors_or_authenticated
     unless Author.count == 0 || current_user
       redirect_to root_path
-      return false
+      false
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_author
-      @author = Author.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def author_params
-      params.require(:author).permit(:username, :email, :password, :password_confirmation)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def author_params
+    params.require(:author).permit(:username, :email, :password, :password_confirmation)
+  end
 end
